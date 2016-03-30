f7491d89609d7b6a5ed786fa84066a0d33f9407b
ambar <ambar.swadesh@gmail.com>
2016-03-30 22:28:02 +0530

    sasa
    asas

---
+++ AjaxForm/AjaxForm/Controllers/AjaxFromController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxForm.Controllers
+{
+    public class AjaxFromController : Controller
+    {
+        //
+        // GET: /AjaxFrom/
+
+        public ActionResult AjaxFrom()
+        {
+            AjaxClass cls = new AjaxClass();
+            cls.str = "";
+    
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AjaxFrom(AjaxClass obj)
+        {
+        //  ViewBag.Title = "ajax";
+            return Content("ajax"+obj .chkval .ToString ()) ;
+        }
+    }
+}
---
+++ AjaxForm/AjaxForm/Models/AjaxClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxForm
+{
+    public class AjaxClass
+    {
+
+        public bool chkval { get; set; }
+        public string str { get; }
+    }
+}
